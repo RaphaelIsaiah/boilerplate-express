05f14a0a963ab3665ae9b6a0741ca663b472d92a
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-11 14:26:43 +0100

    fix: correct comments and remove duplicate static file serving code in myApp.js

--- myApp.js
+++ myApp.js
@@ -1,14 +1,22 @@
+// Setup Express app; initialize an app instance
 let express = require("express");
 let app = express();
 
-// Add the route to serve "Hello Express"
+// Add the route to serve index.html from the views folder
 app.get("/", function (req, res) {
   // res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// Add route to serve static files from the public folder
+app.use("/public", express.static(__dirname + "/public"));
+
+// Add the get route for /json
 app.get("/json", function (req, res) {
   res.json({ message: "Hello json" });
 });
-app.use("/public", express.static(__dirname + "/public"));
+
 // console.log("Hello World");
+
+// Export the app object to enable integration with other files (like server.js)
 module.exports = app;
